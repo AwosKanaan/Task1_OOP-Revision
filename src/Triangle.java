b887ad15a468c324962e37a0ba4ea04222700f6b
AwosKanaan <Awoskanaan55@gmail.com>
2023-10-31 11:33:24 +0300

    adding new overload method and replace whitespaces with tabs

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,72 +1,80 @@
 public class Triangle extends Shape {
-    private double sideA;
-    private double sideB;
-    private double sideC;
+	private double sideA;
+	private double sideB;
+	private double sideC;
 
-    public double getSideA() {
-        return sideA * 1;
-    }
+	public double getSideA() {
+		return sideA * 1;
+	}
 
-    public void setSideA(double sideA) {
-        if(sideA <= 0) {
-            throw new IllegalArgumentException("Triangle side must be greater than 0");
-        }
+	public void setSideA(double sideA) {
+		if (sideA <= 0) {
+			throw new IllegalArgumentException("Triangle side must be greater than 0");
+		}
 
-        this.sideA = sideA;
-    }
+		this.sideA = sideA;
+	}
 
-    public double getSideB() {
-        return sideB;
-    }
+	public double getSideB() {
+		return sideB;
+	}
 
-    public void setSideB(double sideB) {
-        if(sideB <= 0) {
-            throw new IllegalArgumentException("Triangle side c must be greater than 0");
-        }
+	public void setSideB(double sideB) {
+		if (sideB <= 0) {
+			throw new IllegalArgumentException("Triangle side c must be greater than 0");
+		}
 
-        this.sideB = sideB;
-    }
+		this.sideB = sideB;
+	}
 
-    public double getSideC() {
-        return sideC;
-    }
+	public double getSideC() {
+		return sideC;
+	}
 
-    public void setSideC(double sideC) {
-        if(sideC <= 0) {
-            throw new IllegalArgumentException("Triangle side b must be greater than 0");
-        }
+	public void setSideC(double sideC) {
+		if (sideC <= 0) {
+			throw new IllegalArgumentException("Triangle side b must be greater than 0");
+		}
 
-        this.sideC = sideC;
-    }
+		this.sideC = sideC;
+	}
 
-    /**
-     * Calculates the area of the triangle
-     * This method calculates the area of the triangle based on sideA, SideB and SideC
-     * @return area
-     */
-    @Override
-    public double calculateArea() {
-        double s = (sideA + sideB + sideC) / 2;
-        return Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
-    }
+	/**
+	 * Calculates the area of the triangle
+	 * This method calculates the area of the triangle based on sideA, SideB and SideC
+	 *
+	 * @return area
+	 */
+	@Override
+	public double calculateArea() {
+		double s = (sideA + sideB + sideC) / 2;
+		return Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+	}
 
-    /**
-     * Calculates the perimeter of the triangle
-     * This method calculates the area of the triangle based on sideA, sideB, sideC
-     * @return area
-     */
-    @Override
-    public double calculatePerimeter() {
-        return sideA + sideB + sideC;
-    }
-    @Override
-    public String toString() {
-        return "Triangle {" +
-                "sideA = " + sideA + " " +
-                ", sideB = " + sideB + " " +
-                ", sideC = " + sideC + " " +
-                "Area = " + calculateArea() + " " +
-                "Perimeter = " + calculatePerimeter() +
-                '}';
-    }
+	public double calculateArea(double sideA, double sideB, double sideC) {
+		double s = (sideA + sideB + sideC) / 2;
+		return Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+	}
+
+	/**
+	 * Calculates the perimeter of the triangle
+	 * This method calculates the area of the triangle based on sideA, sideB, sideC
+	 *
+	 * @return area
+	 */
+	@Override
+	public double calculatePerimeter() {
+		return sideA + sideB + sideC;
+	}
+
+	@Override
+	public String toString() {
+		return "Triangle {" +
+				"sideA = " + sideA + " " +
+				", sideB = " + sideB + " " +
+				", sideC = " + sideC + " " +
+				"Area = " + calculateArea() + " " +
+				"Perimeter = " + calculatePerimeter() +
+				'}';
+	}
 }
