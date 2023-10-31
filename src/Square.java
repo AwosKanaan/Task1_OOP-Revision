708b5de1f00c00ec5b2ea15b01f5cc14bc866746
AwosKanaan <Awoskanaan55@gmail.com>
2023-10-31 11:58:35 +0300

    Updated the indentation from whitespaces to tabs in the other files, corrected and added java docs for methods, added overloaded methods for the other classes

--- src/Square.java
+++ src/Square.java
@@ -1,43 +1,55 @@
 public class Square extends Shape {
-    private double side;
+	private double side;
 
-    public double getSide() {
-        return side;
-    }
+	public double getSide() {
+		return side;
+	}
 
-    public void setSide(double side) {
-        if (side <= 0) {
-            throw new IllegalArgumentException("Square side must be greater than 0");
-        }
-        this.side = side;
-    }
+	public void setSide(double side) {
+		if (side <= 0) {
+			throw new IllegalArgumentException("Square side must be greater than 0");
+		}
+		this.side = side;
+	}
 
-    /**
-     * Calculates the area of the square
-     * This method calculates the area of the square based on the side
-     * @return area
-     */
-    @Override
-    public double calculateArea() {
-        return 4 * side;
-    }
+	/**
+	 * Calculates the area of the square
+	 * This method calculates the area of the square based on attribute(s) (side)
+	 *
+	 * @return area
+	 */
+	@Override
+	public double calculateArea() {
+		return 4 * side;
+	}
 
-    /**
-     * Calculates the perimeter of the square
-     * This method calculates the perimeter of the square based on the side
-     * @return perimeter
-     */
-    @Override
-    public double calculatePerimeter() {
-        return side * side;
-    }
+	/**
+	 * Calculates the area of the square
+	 * This method calculates the area of the square based on parameter(s) (side)
+	 * @param side
+	 * @return area
+	 */
+	public double calculateArea(double side) {
+		return 4 * side;
+	}
 
-    @Override
-    public String toString() {
-        return "Square {" +
-                "side = " + side + " " +
-                "Area = " + calculateArea() + " " +
-                "Perimeter = " + calculatePerimeter() +
-                '}';
-    }
+	/**
+	 * Calculates the perimeter of the square
+	 * This method calculates the perimeter of the square based on attribute(s) (side)
+	 *
+	 * @return perimeter
+	 */
+	@Override
+	public double calculatePerimeter() {
+		return side * side;
+	}
+
+	@Override
+	public String toString() {
+		return "Square {" +
+				"side = " + side + " " +
+				"Area = " + calculateArea() + " " +
+				"Perimeter = " + calculatePerimeter() +
+				'}';
+	}
 }
